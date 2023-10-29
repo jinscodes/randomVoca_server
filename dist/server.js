03d3a956ea1beb1a72998fcdd1dacfac93f241db
{jinsung} <jayhan0215@gmail.com>
2023-10-30 01:08:03 +0900

    Trying to get datas from notion's database

--- dist/server.js
+++ dist/server.js
@@ -1,27 +1,134 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@notionhq/client");
+require("dotenv/config");
 const http_1 = __importDefault(require("http"));
+// The dotenv library will read from your .env file into these values on `process.env`
+const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+const notionSecret = process.env.NOTION_SECRET;
+// Will provide an error to users who forget to create the .env file
+// with their Notion data in it
+if (!notionDatabaseId || !notionSecret) {
+    throw Error("Must define NOTION_SECRET and NOTION_DATABASE_ID in env");
+}
+// Initializing the Notion client with your secret
+const notion = new client_1.Client({
+    auth: notionSecret,
+});
 const host = "localhost";
 const port = 8000;
-const server = http_1.default.createServer((req, res) => {
-    // Avoid CORS errors
+// Require an async function here to support await with the DB query
+const server = http_1.default.createServer((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Content-Type", "application/json");
     switch (req.url) {
-        // Will respond to queries to the domain root (like http://localhost/)
         case "/":
+            // Query the database and wait for the result
+            const query = yield notion.databases.query({
+                database_id: notionDatabaseId,
+            });
+            // console.log("query: ", query);
+            query.results.map((row) => {
+                // console.log(row.properties);
+                // console.log("title: ", row.properties.title);
+                if (row.properties.title.type === "rich_text" &&
+                    row.properties.title.rich_text[0]) {
+                    console.log("title rich_text: ", row.properties.title.rich_text[0]);
+                }
+            });
+            // We map over the complex shape of the results and return a nice clean array of
+            // objects in the shape of our `ThingToLearn` interface
+            // const list: ThingToLearn[] = query.results.map((row) => {
+            //   // console.log("row: ", row);
+            //   // row represents a row in our database and the name of the column is the
+            //   // way to reference the data in that column
+            //   const titleCell = row.properties.title;
+            //   const chapterCell = row.properties.chapter;
+            //   const words_enCell = row.properties.words_en;
+            //   const words_koCell = row.properties.words_ko;
+            //   const correctCell = row.properties.correct;
+            //   const wrongCell = row.properties.wrong;
+            //   // Depending on the column "type" we selected in Notion there will be different
+            //   // data available to us (URL vs Date vs text for example) so in order for Typescript
+            //   // to safely infer we have to check the `type` value.  We had one text and one url column.
+            //   const isTitle = titleCell.type === "rich_text";
+            //   const isChapter = chapterCell.type === "rich_text";
+            //   const isWords_en = words_enCell.type === "rich_text";
+            //   const isWords_ko = words_koCell.type === "rich_text";
+            //   const isCorrect = correctCell.type === "number";
+            //   const isWrong = wrongCell.type === "number";
+            //   // Verify the types are correct
+            //   if (
+            //     isTitle &&
+            //     isChapter &&
+            //     isWords_en &&
+            //     isWords_ko &&
+            //     isCorrect &&
+            //     isWrong
+            //   ) {
+            //     // Pull the string values of the cells off the column data
+            //     // const url = urlCell.url ?? "";
+            //     // console.log("titleCell: ", titleCell);
+            //     // console.log("title rich_text: ", titleCell.rich_text);
+            //     const title = titleCell.rich_text?.[0].plain_text;
+            //     const chapter = chapterCell.rich_text?.[0].plain_text;
+            //     const words_en = words_enCell.rich_text?.[0].plain_text;
+            //     const words_ko = words_koCell.rich_text?.[0].plain_text;
+            //     const correct = correctCell.number;
+            //     const wrong = wrongCell.number;
+            //     // console.log("title: ", title);
+            //     // console.log("correct: ", correct);
+            //     // console.log("wrong: ", wrong);
+            //     // Return it in our `ThingToLearn` shape
+            //     return {
+            //       title,
+            //       chapter,
+            //       words_en,
+            //       words_ko,
+            //       // correct,
+            //       // wrong,
+            //     };
+            //   }
+            //   // If a row is found that does not match the rules we checked it will still return in the
+            //   // the expected shape but with a NOT_FOUND label
+            //   return {
+            //     title: "NOT_FOUND",
+            //     chapter: "NOT_FOUND",
+            //     words_en: "NOT_FOUND",
+            //     words_ko: "NOT_FOUND",
+            //     correct: 0,
+            //     wrong: 0,
+            //   };
+            // });
+            let list = {
+                title: "NOT_FOUND",
+                chapter: "NOT_FOUND",
+                words_en: "NOT_FOUND",
+                words_ko: "NOT_FOUND",
+                correct: 0,
+                wrong: 0,
+            };
+            res.setHeader("Content-Type", "application/json");
             res.writeHead(200);
-            res.end(JSON.stringify({ data: "success" }));
+            res.end(JSON.stringify(list));
             break;
-        // Only supports the / route
         default:
+            res.setHeader("Content-Type", "application/json");
             res.writeHead(404);
             res.end(JSON.stringify({ error: "Resource not found" }));
     }
-});
+}));
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-require("dotenv").config();
 import { Client } from "@notionhq/client";
+import "dotenv/config";
 import http from "http";
 
 // This is Typescript  interface for the shape of the object we will
@@ -7,9 +7,12 @@ import http from "http";
 // When the data is queried it will come back in a much more complicated shape, so our goal is to
 // simplify it to make it easy to work with on the front end
 interface ThingToLearn {
-  label: string;
-  url: string;
   title: string;
+  chapter: string;
+  words_en: string;
+  words_ko: string;
+  correct?: number;
+  wrong?: number;
 }
 
 // The dotenv library will read from your .env file into these values on `process.env`
@@ -41,37 +44,101 @@ const server = http.createServer(async (req, res) => {
         database_id: notionDatabaseId,
       });
 
+      // console.log("query: ", query);
+
+      query.results.map((row) => {
+        // console.log(row.properties);
+        // console.log("title: ", row.properties.title);
+
+        if (
+          row.properties.title.type === "rich_text" &&
+          row.properties.title.rich_text[0]
+        ) {
+          console.log("title rich_text: ", row.properties.title.rich_text[0]);
+        }
+      });
+
       // We map over the complex shape of the results and return a nice clean array of
       // objects in the shape of our `ThingToLearn` interface
-      const list: ThingToLearn[] = query.results.map((row) => {
-        // row represents a row in our database and the name of the column is the
-        // way to reference the data in that column
-        const labelCell = row.properties.label;
-        const urlCell = row.properties.url;
-        const titleCell = row.properties.title;
+      // const list: ThingToLearn[] = query.results.map((row) => {
+      //   // console.log("row: ", row);
 
-        // Depending on the column "type" we selected in Notion there will be different
-        // data available to us (URL vs Date vs text for example) so in order for Typescript
-        // to safely infer we have to check the `type` value.  We had one text and one url column.
-        const isLabel = labelCell.type === "rich_text";
-        const isUrl = urlCell.type === "url";
-        const isTitle = titleCell.type === "rich_text";
+      //   // row represents a row in our database and the name of the column is the
+      //   // way to reference the data in that column
+      //   const titleCell = row.properties.title;
+      //   const chapterCell = row.properties.chapter;
+      //   const words_enCell = row.properties.words_en;
+      //   const words_koCell = row.properties.words_ko;
+      //   const correctCell = row.properties.correct;
+      //   const wrongCell = row.properties.wrong;
 
-        // Verify the types are correct
-        if (isLabel && isUrl && isTitle) {
-          // Pull the string values of the cells off the column data
-          const label = labelCell.rich_text?.[0].plain_text;
-          const url = urlCell.url ?? "";
-          const title = titleCell.rich_text?.[0].plain_text;
+      //   // Depending on the column "type" we selected in Notion there will be different
+      //   // data available to us (URL vs Date vs text for example) so in order for Typescript
+      //   // to safely infer we have to check the `type` value.  We had one text and one url column.
+      //   const isTitle = titleCell.type === "rich_text";
+      //   const isChapter = chapterCell.type === "rich_text";
+      //   const isWords_en = words_enCell.type === "rich_text";
+      //   const isWords_ko = words_koCell.type === "rich_text";
+      //   const isCorrect = correctCell.type === "number";
+      //   const isWrong = wrongCell.type === "number";
 
-          // Return it in our `ThingToLearn` shape
-          return { label, url, title };
-        }
+      //   // Verify the types are correct
+      //   if (
+      //     isTitle &&
+      //     isChapter &&
+      //     isWords_en &&
+      //     isWords_ko &&
+      //     isCorrect &&
+      //     isWrong
+      //   ) {
+      //     // Pull the string values of the cells off the column data
+      //     // const url = urlCell.url ?? "";
 
-        // If a row is found that does not match the rules we checked it will still return in the
-        // the expected shape but with a NOT_FOUND label
-        return { label: "NOT_FOUND", url: "", title: "" };
-      });
+      //     // console.log("titleCell: ", titleCell);
+      //     // console.log("title rich_text: ", titleCell.rich_text);
+
+      //     const title = titleCell.rich_text?.[0].plain_text;
+      //     const chapter = chapterCell.rich_text?.[0].plain_text;
+      //     const words_en = words_enCell.rich_text?.[0].plain_text;
+      //     const words_ko = words_koCell.rich_text?.[0].plain_text;
+      //     const correct = correctCell.number;
+      //     const wrong = wrongCell.number;
+
+      //     // console.log("title: ", title);
+      //     // console.log("correct: ", correct);
+      //     // console.log("wrong: ", wrong);
+
+      //     // Return it in our `ThingToLearn` shape
+      //     return {
+      //       title,
+      //       chapter,
+      //       words_en,
+      //       words_ko,
+      //       // correct,
+      //       // wrong,
+      //     };
+      //   }
+
+      //   // If a row is found that does not match the rules we checked it will still return in the
+      //   // the expected shape but with a NOT_FOUND label
+      //   return {
+      //     title: "NOT_FOUND",
+      //     chapter: "NOT_FOUND",
+      //     words_en: "NOT_FOUND",
+      //     words_ko: "NOT_FOUND",
+      //     correct: 0,
+      //     wrong: 0,
+      //   };
+      // });
+
+      let list = {
+        title: "NOT_FOUND",
+        chapter: "NOT_FOUND",
+        words_en: "NOT_FOUND",
+        words_ko: "NOT_FOUND",
+        correct: 0,
+        wrong: 0,
+      };
 
       res.setHeader("Content-Type", "application/json");
       res.writeHead(200);
